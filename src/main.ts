efaba695438c46f06f07f3e72534f2e8faf5f9a0
Dedestem <david@davidnet.net>
2025-03-02 09:35:29 +0100

    IT WORKS - Formatting & Linting

--- src/main.ts
+++ src/main.ts
@@ -325,7 +325,6 @@ app.use(async (ctx) => {
         ctx.response.body = { message: "ok" };
     }
 
-
     // Get session
     if (
         ctx.request.method === "POST" &&
@@ -481,53 +480,58 @@ app.use(async (ctx) => {
         ctx.request.method === "POST" &&
         ctx.request.url.pathname === "/delete_session"
     ) {
-        const body = await ctx.request.body().value as { token?: string, session_id?: string };
-    
+        const body = await ctx.request.body().value as {
+            token?: string;
+            session_id?: string;
+        };
+
         if (!body.token) {
             ctx.response.status = 400;
             ctx.response.body = { error: "Missing token" };
             return;
         }
-    
+
         if (!body.session_id) {
             ctx.response.status = 400;
             ctx.response.body = { error: "Missing session_id" };
             return;
         }
-    
+
         // Haal de gebruiker op die hoort bij het huidige token
         const userSession = await db.query(
             "SELECT userid FROM sessions WHERE token = ?",
             [body.token],
         );
-    
+
         if (userSession.length === 0) {
             ctx.response.status = 400;
             ctx.response.body = { error: "Invalid session token" };
             return;
         }
-    
+
         const userId = userSession[0].userid;
-    
+
         // Controleer of de sessie die verwijderd moet worden van dezelfde gebruiker is
         const sessionCheck = await db.query(
             "SELECT id FROM sessions WHERE id = ? AND userid = ?",
             [body.session_id, userId],
         );
-    
+
         if (sessionCheck.length === 0) {
             ctx.response.status = 400;
             ctx.response.body = { error: "unauthorized" };
             return;
         }
-    
+
         // Verwijder de sessie
-        await db.execute("DELETE FROM sessions WHERE id = ?", [body.session_id]);
-    
+        await db.execute("DELETE FROM sessions WHERE id = ?", [
+            body.session_id,
+        ]);
+
         ctx.response.status = 200;
         ctx.response.body = { message: "ok" };
     }
-    
+
     if (
         ctx.request.method === "POST" &&
         ctx.request.url.pathname === "/get_username"
@@ -567,8 +571,7 @@ app.use(async (ctx) => {
 
         ctx.response.body = { message: "ok", username: username };
     }
-});    
-    
+});
 
 // Start the server
 console.log(`Server running at http://localhost:${port}`);
