674dfc7abbfed5a3d633e7cf1f150ce4528909a3
Dedestem <david@davidnet.net>
2025-03-03 21:33:12 +0100

    Test

--- src/main.ts
+++ src/main.ts
@@ -1081,6 +1081,41 @@ app.use(async (ctx) => {
 
         ctx.response.body = { message: "ok"};
     }
+
+    if (
+        ctx.request.method === "POST" &&
+        ctx.request.url.pathname === "/set_up_totp"
+    ) {
+        const body = await ctx.request.body().value as { token?: string, secret?: string };
+
+        if (!body.token || !body.secret) {
+            ctx.response.status = 400;
+            ctx.response.body = { error: "Missing fields" };
+            return;
+        }
+
+        const sessionResult = await db.query(
+            "SELECT userid FROM sessions WHERE token = ?",
+            [body.token],
+        );
+
+        if (sessionResult.length === 0) {
+            ctx.response.status = 400;
+            ctx.response.body = { error: "Invalid session token" };
+            return;
+        }
+
+        const userid = sessionResult[0].userid;
+
+        await db.query(
+            "UPDATE users SET totp_seed = ?, totp_enabled = 1 WHERE id = ?",
+            [body.secret, userid],
+        );
+
+        ctx.response.body = {
+            message: "ok",
+        };
+    }
 });
 
 // Start the server
