512a78fa775f4e0ba3233bc8bc0a12603903038d
dedestem <david@davidnet.net>
2025-08-29 23:39:02 +0200

    Fix cors

--- api/middlewares/cors.ts
+++ api/middlewares/cors.ts
@@ -1,4 +1,5 @@
 import { Middleware } from "https://deno.land/x/oak@v12.6.1/mod.ts";
+import { log_error } from "../lib/logger.ts";
 
 const DA_ISPROD = Deno.env.get("DA_ISPROD") === "true";
 
@@ -20,7 +21,7 @@ export const cors: Middleware = async (ctx, next) => {
     }
   }
 
-  // Always set headers if we decided to allow this origin
+  // Always set CORS headers if allowOrigin is set
   if (allowOrigin) {
     ctx.response.headers.set("Access-Control-Allow-Origin", allowOrigin);
     ctx.response.headers.set(
@@ -34,13 +35,33 @@ export const cors: Middleware = async (ctx, next) => {
     ctx.response.headers.set("Access-Control-Allow-Credentials", "true");
   }
 
-  // Preflight OPTIONS response
+  // OPTIONS preflight
   if (ctx.request.method === "OPTIONS") {
     ctx.response.status = 204;
     return;
   }
 
-  await next();
+  try {
+    await next();
+  } catch (err) {
+    // Ensure CORS headers are still set on errors
+    if (allowOrigin) {
+      ctx.response.headers.set("Access-Control-Allow-Origin", allowOrigin);
+      ctx.response.headers.set(
+        "Access-Control-Allow-Methods",
+        "GET,POST,PUT,DELETE,OPTIONS"
+      );
+      ctx.response.headers.set(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization, x-correlation-id"
+      );
+      ctx.response.headers.set("Access-Control-Allow-Credentials", "true");
+    }
+
+    ctx.response.status = 500;
+    ctx.response.body = { error: "Unknown CORS error." };
+    log_error(err, "CORS middleware error");
+  }
 };
 
 export default cors;
