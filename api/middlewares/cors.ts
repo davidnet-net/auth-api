4fc4e9f72659a21e9100ec5a4f813c6ec13fc400
dedestem <david@davidnet.net>
2025-08-29 23:14:29 +0200

    Improve handling of CORS

--- api/middlewares/cors.ts
+++ api/middlewares/cors.ts
@@ -1,33 +1,38 @@
 import { Middleware } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 
 const DA_ISPROD = Deno.env.get("DA_ISPROD") === "true";
-if (typeof DA_ISPROD !== "boolean") {
-    throw new Error("Invalid env: DA_ISPROD");
-}
 
 export const cors: Middleware = async (ctx, next) => {
-    const requestOrigin = ctx.request.headers.get("origin");
+  const requestOrigin = ctx.request.headers.get("origin");
 
-    if (requestOrigin && DA_ISPROD) {
-        const url = new URL(requestOrigin);
-        const hostname = url.hostname;
+  if (requestOrigin) {
+    try {
+      const url = new URL(requestOrigin);
+      const hostname = url.hostname;
 
-        if (hostname === "davidnet.net" || hostname.endsWith(".davidnet.net")) {
-            ctx.response.headers = new Headers({
-            "Access-Control-Allow-Origin": requestOrigin,
-            "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
-            "Access-Control-Allow-Headers": "Content-Type, Authorization, x-correlation-id",
-            "Access-Control-Allow-Credentials": "true"
-            });
-        }
+      if (!DA_ISPROD || hostname === "davidnet.net" || hostname.endsWith(".davidnet.net")) {
+        ctx.response.headers.set("Access-Control-Allow-Origin", requestOrigin);
+        ctx.response.headers.set(
+          "Access-Control-Allow-Methods",
+          "GET,POST,PUT,DELETE,OPTIONS"
+        );
+        ctx.response.headers.set(
+          "Access-Control-Allow-Headers",
+          "Content-Type, Authorization, x-correlation-id"
+        );
+        ctx.response.headers.set("Access-Control-Allow-Credentials", "true");
+      }
+    } catch (_err) {
+      // Invalid origin URL; just ignore
     }
+  }
 
-    if (ctx.request.method === "OPTIONS") {
-        ctx.response.status = 204;
-        return;
-    }
+  if (ctx.request.method === "OPTIONS") {
+    ctx.response.status = 204;
+    return;
+  }
 
-    await next();
+  await next();
 };
 
 export default cors;
