8eb575c7b986aa87f48aa709775b677a484ba4eb
dedestem <david@davidnet.net>
2025-09-05 19:54:46 +0200

    Update placeholder pic to prod

--- api/controllers/signup.ts
+++ api/controllers/signup.ts
@@ -9,7 +9,7 @@ import { formatDateWithUTCOffset } from "../lib/time.ts";
 import { getForwardedIP } from "../lib/internet.ts";
 
 const AVATAR_PLACEHOLDER =
-	"http://localhost:5173/placeholder.png";
+	"https://davidnet.net/placeholder.png";
 const DA_ISPROD = Deno.env.get("DA_ISPROD") === "true";
 if (typeof DA_ISPROD !== "boolean") {
 	throw new Error("Invalid env: DA_ISPRO");
